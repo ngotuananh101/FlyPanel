1ac6e5a3fea7ab17491d3cde3485e5848fac0e84
Ngo Tuan Anh <ngotuananh2101@gmail.com>
2025-05-05 17:34:02 +0700

    feat: enhance layout styles, add chart initialization script, and update theme settings

---
+++ FlyPanel/wwwroot/assets/js/custom/home.js
@@ -0,0 +1,48 @@
+﻿window.initializeCharts = function () {
+    const cpuOptions = {
+        chart: {
+            height: 160,
+            width: 160,
+            type: "radialBar"
+        },
+        series: [0],
+        plotOptions: {
+            radialBar: {
+                hollow: {
+                    margin: 20,
+                    size: "60%"
+                },
+                dataLabels: {
+                    showOn: "always",
+                    name: { show: false },
+                    value: {
+                        offsetY: 10,
+                        color: "#111",
+                        fontSize: "20px",
+                        show: true
+                    }
+                }
+            }
+        },
+        stroke: { lineCap: "round" },
+        labels: ["CPU Usage"]
+    };
+
+    const ramOptions = {
+        ...cpuOptions,
+        labels: ["RAM Usage"]
+    };
+
+    window.chartCPU = new ApexCharts(document.querySelector("#chartCPU"), cpuOptions);
+    window.chartRAM = new ApexCharts(document.querySelector("#chartRAM"), ramOptions);
+
+    chartCPU.render();
+    chartRAM.render();
+};
+
+window.updateCharts = function (cpuUsage, ramUsage) {
+    if (window.chartCPU && window.chartRAM) {
+        window.chartCPU.updateSeries([cpuUsage]);
+        window.chartRAM.updateSeries([ramUsage]);
+    }
+};
