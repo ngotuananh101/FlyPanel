6ae12804d8497cdbe257db074b6bb6bd127aedb7
Ngo Tuan Anh <ngotuananh2101@gmail.com>
2025-05-17 03:15:39 +0700

    init style

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>To get started, edit <code>src/App.vue</code> and save to reload.</p>
+    <p>For more information, visit <a href="https://vuejs.org">Vue.js</a>.</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
